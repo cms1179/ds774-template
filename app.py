db145412679d13ea1069d4ff8676ed1a555459e4
Catherine Swift <cms1179@wildcats.unh.edu>
2022-03-21 23:18:12 -0400

    sfs

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ app.secret_key = "IAN"
 def home():
     return render_template('home.html')
 
+@app.route("/announcements")
+def announcements():
+    return render_template('announcements.html')
+
 @app.route("/directory")
 def directory():
     return render_template('directory.html')
